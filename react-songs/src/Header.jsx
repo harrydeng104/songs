a53618a73c8acc7f9afea5c16a1adae9e7dd2809
harrydeng104 <harrydeng104@gmail.com>
2025-07-05 22:11:17 -0400

    header

---
+++ react-songs/src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <h1>SongTrack</h1>
+            <nav>
+                <ul>
+                    <li>Main</li>
+                    <li>Info</li>
+                    <li>Eval</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
